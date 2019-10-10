bd272a846beed0f6728c29ba93c878617485974c
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-10-10 09:55:19 -0400

    fix migrate table for category name

--- data/migrations/20190504170606_categories.js
+++ data/migrations/20190504170606_categories.js
@@ -2,7 +2,7 @@ exports.up = function (knex, Promise) {
   return knex.schema.createTable("categories", function (categories) {
     categories.increments();
     categories
-      .text("name", 255)
+      .text("cat_name", 255)
       .notNullable()
       .unique();
     categories.text("logo", 255).unique();

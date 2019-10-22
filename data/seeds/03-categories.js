d8db7b64cbe1eb5a1f3379cf07d6c6a52e0caa52
Alex Bezruchko <alex.bezruchko@gmail.com>
2019-10-22 09:58:45 -0400

    add category slug, add products fetchByCategory query

--- data/seeds/03-categories.js
+++ data/seeds/03-categories.js
@@ -1,12 +1,33 @@
 exports.seed = function (knex, Promise) {
   return knex("categories").insert([
-    { cat_name: "supplements" },
-    { cat_name: "herbs" },
-    { cat_name: "spices" },
-    { cat_name: "bulk tea" },
-    { cat_name: "ointments" },
-    { cat_name: "bath & beauty" },
-    { cat_name: "books" },
+    {
+      cat_name: "supplements",
+      cat_slug: "supplements"
+    },
+    {
+      cat_name: "herbs",
+      cat_slug: "herbs"
+    },
+    {
+      cat_name: "spices",
+      cat_slug: "spices"
+    },
+    {
+      cat_name: "bulk tea",
+      cat_slug: "bulk"
+    },
+    {
+      cat_name: "ointments",
+      cat_slug: "ointments"
+    },
+    {
+      cat_name: "bath & beauty",
+      cat_slug: "bath"
+    },
+    {
+      cat_name: "books",
+      cat_slug: "books"
+    },
 
   ]);
 };
